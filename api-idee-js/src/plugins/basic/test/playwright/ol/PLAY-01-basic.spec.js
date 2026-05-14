2c27a93b29d4dfc4e8351bac3ff6f89f2bc79746
andrestcpy <andresmiras@grupotecopy.es>
2026-05-14 10:16:34 +0200

    feat: test of plugin basic is not avaliable in 2.0 test is commented

--- api-idee-js/src/plugins/basic/test/playwright/ol/PLAY-01-basic.spec.js
+++ api-idee-js/src/plugins/basic/test/playwright/ol/PLAY-01-basic.spec.js
@@ -1,17 +1,17 @@
-import { test, expect } from '@playwright/test';
+// import { test, expect } from '@playwright/test';
 
-test('Test Basic', async ({ page }) => {
-  await page.goto('/src/plugins/basic/test/playwright/ol/basic-ol.html');
-  await page.evaluate(() => {
-    window.mapjs = IDEE.map({
-      container: 'mapjs',
-    });
-    window.mp = new IDEE.plugin.Basic({
-      position: 'left',
-    });
-    window.mapjs.addPlugin(window.mp);
-  });
+// test('Plugin Test Basic', async ({ page }) => {
+//   await page.goto('/src/plugins/basic/test/playwright/ol/basic-ol.html');
+//   await page.evaluate(() => {
+//     window.mapjs = IDEE.map({
+//       container: 'mapjs',
+//     });
+//     window.mp = new IDEE.plugin.Basic({
+//       position: 'left',
+//     });
+//     window.mapjs.addPlugin(window.mp);
+//   });
 
-  const nPlugins = await page.evaluate(() => window.mapjs.getPlugins().length);
-  expect(nPlugins).toBe(1);
-});
+//   const nPlugins = await page.evaluate(() => window.mapjs.getPlugins().length);
+//   expect(nPlugins).toBe(1);
+// });
