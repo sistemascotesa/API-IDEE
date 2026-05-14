f56761d6d39af09c280918ab232f061c1bad7f56
Alberto <albertosamaniego00@gmail.com>
2026-05-14 18:10:41 +0200

    feat: rebuild plugin mousesrs and viewmanagement tests

--- api-idee-js/src/plugins/viewmanagement/test/playwright/ol/PLAY-01-viewmanagement.spec.js
+++ api-idee-js/src/plugins/viewmanagement/test/playwright/ol/PLAY-01-viewmanagement.spec.js
@@ -1,17 +1,15 @@
 import { test, expect } from '@playwright/test';
 
-test('Test viewmanagement', async ({ page }) => {
+test('Test plugin Viewmanagement', async ({ page }) => {
   await page.goto('/src/plugins/viewmanagement/test/playwright/ol/viewmanagement-ol.html');
   await page.evaluate(() => {
     window.mapjs = IDEE.map({
       container: 'mapjs',
     });
-    window.mp = new IDEE.plugin.ViewManagement({
-      position: 'left',
-    });
+    window.mp = new IDEE.plugin.ViewManagement();
     window.mapjs.addPlugin(window.mp);
   });
   
-  const nPlugins = await page.evaluate(() => window.mapjs.getPlugins().length);
+  const nPlugins = await page.evaluate(() => window.mapjs.getPlugins(window.mp.name).length);
   expect(nPlugins).toBe(1);
 });
