eb89f1f5d6aa4f603da356c94dcf272f074664d3
andrestcpy <andresmiras@grupotecopy.es>
2026-05-14 11:10:30 +0200

    feat: rebuild plugin vectorsmanagement test

--- api-idee-js/src/plugins/vectorsmanagement/test/playwright/ol/PLAY-01-vectorsmanagement.spec.js
+++ api-idee-js/src/plugins/vectorsmanagement/test/playwright/ol/PLAY-01-vectorsmanagement.spec.js
@@ -1,17 +1,15 @@
 import { test, expect } from '@playwright/test';
 
-test('Test vectorsmanagement', async ({ page }) => {
+test('Test Plugin VectorsManagement', async ({ page }) => {
   await page.goto('/src/plugins/vectorsmanagement/test/playwright/ol/vectorsmanagement-ol.html');
   await page.evaluate(() => {
     window.mapjs = IDEE.map({
       container: 'mapjs',
     });
-      window.mp = new IDEE.plugin.VectorsManagement({
-      position: 'BR'
-    });
+    window.mp = new IDEE.plugin.VectorsManagement();
     window.mapjs.addPlugin(window.mp);
   });
-  
-  const nPlugins = await page.evaluate(() => window.mapjs.getPlugins().length);
+
+  const nPlugins = await page.evaluate(() => window.mapjs.getPlugins(window.mp.name).length);
   expect(nPlugins).toBe(1);
 });
