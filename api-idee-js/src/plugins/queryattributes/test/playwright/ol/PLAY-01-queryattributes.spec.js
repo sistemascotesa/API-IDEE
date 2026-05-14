a589cc190898c8b8cac7fef27f1c486b4faf2f51
andrestcpy <andresmiras@grupotecopy.es>
2026-05-14 10:23:51 +0200

    feat: test of plugin queryattributes is not avaliable in 2.0 test is commented

--- api-idee-js/src/plugins/queryattributes/test/playwright/ol/PLAY-01-queryattributes.spec.js
+++ api-idee-js/src/plugins/queryattributes/test/playwright/ol/PLAY-01-queryattributes.spec.js
@@ -1,18 +1,18 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
-import { test, expect } from '@playwright/test';
+// // eslint-disable-next-line import/no-extraneous-dependencies
+// import { test, expect } from '@playwright/test';
 
-test('Test Plugin QueryAttributes', async ({ page }) => {
-  await page.goto('/src/plugins/queryattributes/test/playwright/ol/queryattributes-ol.html');
-  await page.evaluate(() => {
-    window.mapjs = IDEE.map({
-      container: 'mapjs',
-    });
-    window.mp = new IDEE.plugin.QueryAttributes({
-      position: 'TL',
-    });
-    window.mapjs.addPlugin(window.mp);
-  });
+// test('Test Plugin QueryAttributes', async ({ page }) => {
+//   await page.goto('/src/plugins/queryattributes/test/playwright/ol/queryattributes-ol.html');
+//   await page.evaluate(() => {
+//     window.mapjs = IDEE.map({
+//       container: 'mapjs',
+//     });
+//     window.mp = new IDEE.plugin.QueryAttributes({
+//       position: 'TL',
+//     });
+//     window.mapjs.addPlugin(window.mp);
+//   });
 
-  const nPlugins = await page.evaluate(() => window.mapjs.getPlugins().length);
-  expect(nPlugins).toBe(1);
-});
+//   const nPlugins = await page.evaluate(() => window.mapjs.getPlugins().length);
+//   expect(nPlugins).toBe(1);
+// });
