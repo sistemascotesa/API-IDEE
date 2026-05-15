e9dfc7d8529741b2e4fcd3b97c8477d1714bd388
Alberto <albertosamaniego00@gmail.com>
2026-05-15 09:31:26 +0200

    feat: rebuild test plugin comparators

--- api-idee-js/src/plugins/comparators/test/playwright/ol/PLAY-01-comparators.spec.js
+++ api-idee-js/src/plugins/comparators/test/playwright/ol/PLAY-01-comparators.spec.js
@@ -1,17 +1,15 @@
 import { test, expect } from '@playwright/test';
 
-test('Test Comparators', async ({ page }) => {
+test('Test plugin Comparators', async ({ page }) => {
   await page.goto('/src/plugins/comparators/test/playwright/ol/comparators-ol.html');
   await page.evaluate(() => {
     window.mapjs = IDEE.map({
       container: 'mapjs',
     });
-    window.mp = new IDEE.plugin.Comparators({
-      position: 'left',
-    });
+    window.mp = new IDEE.plugin.Comparators();
     window.mapjs.addPlugin(window.mp);
   });
   
-  const nPlugins = await page.evaluate(() => window.mapjs.getPlugins().length);
+  const nPlugins = await page.evaluate(() => window.mapjs.getPlugins(window.mp.name).length);
   expect(nPlugins).toBe(1);
 });
