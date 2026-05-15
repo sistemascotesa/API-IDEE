426a182ba479d408d69dc21498d1277ba041add5
Alberto <albertosamaniego00@gmail.com>
2026-05-15 09:32:35 +0200

    feat: rebuild test plugin selectionzoom

--- api-idee-js/src/plugins/selectionzoom/test/playwright/ol/PLAY-01-selectionzoom.spec.js
+++ api-idee-js/src/plugins/selectionzoom/test/playwright/ol/PLAY-01-selectionzoom.spec.js
@@ -1,17 +1,15 @@
 import { test, expect } from '@playwright/test';
 
-test('Test SelectionZoom', async ({ page }) => {
+test('Test plugin SelectionZoom', async ({ page }) => {
   await page.goto('/src/plugins/selectionzoom/test/playwright/ol/selectionzoom-ol.html');
   await page.evaluate(() => {
     window.mapjs = IDEE.map({
       container: 'mapjs',
     });
-    window.mp = new IDEE.plugin.SelectionZoom({
-      position: 'TL',
-    });
+    window.mp = new IDEE.plugin.SelectionZoom();
     window.mapjs.addPlugin(window.mp);
   });
   
-  const nPlugins = await page.evaluate(() => window.mapjs.getPlugins().length);
+  const nPlugins = await page.evaluate(() => window.mapjs.getPlugins(window.mp.name).length);
   expect(nPlugins).toBe(1);
 });
