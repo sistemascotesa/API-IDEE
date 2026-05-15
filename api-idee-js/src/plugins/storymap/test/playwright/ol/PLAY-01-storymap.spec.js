9f356f68acd1ffa0b593d165f75f0fa6e79cafdb
Alberto <albertosamaniego00@gmail.com>
2026-05-15 09:32:44 +0200

    feat: rebuild test plugin storymap

--- api-idee-js/src/plugins/storymap/test/playwright/ol/PLAY-01-storymap.spec.js
+++ api-idee-js/src/plugins/storymap/test/playwright/ol/PLAY-01-storymap.spec.js
@@ -1,60 +1,16 @@
 import { test, expect } from '@playwright/test';
 
-import StoryMapJSON2 from '../../StoryMapJSON2.json';
+test('Test plugin StoryMap', async ({ page }) => {
+    await page.goto('/src/plugins/storymap/test/playwright/ol/storymap-ol.html');
+    await page.evaluate(() => {
+        window.mapjs = IDEE.map({
+            container: 'mapjs',
+        });
 
-import StoryMapJSON1 from '../../StoryMapJSON1.json';
+        window.mp = new IDEE.plugin.StoryMap();
+        window.mapjs.addPlugin(window.mp);
+    });
 
-test('Test storymap', async ({ page }) => {
-
-await page.goto('/src/plugins/storymap/test/playwright/ol/storymap-ol.html');
-
-await page.evaluate(({ contentEs, contentEn }) => {
-
-window.mapjs = IDEE.map({
-
-container: 'mapjs',
-
-});
-
-window.mp = new IDEE.plugin.StoryMap({
-
-collapsed: false,
-
-collapsible: true,
-
-position: 'TR',
-
-tooltip: 'Tooltip Storymap',
-
-content: {
-
-es: contentEs,
-
-en: contentEn,
-
-},
-
-indexInContent: {
-
-title: 'Índice StoryMap',
-
-subtitle: 'Visualizador de Cervantes y el Madrid del siglo XVII',
-
-js: "console.log('Visualizador de Cervantes');",
-
-},
-
-delay: 2000,
-
-});
-
-window.mapjs.addPlugin(window.mp);
-
-}, { contentEs: StoryMapJSON2, contentEn: StoryMapJSON1 });
-
-
-const nPlugins = await page.evaluate(() => window.mapjs.getPlugins().length);
-
-expect(nPlugins).toBe(1);
-
+    const nPlugins = await page.evaluate(() => window.mapjs.getPlugins(window.mp.name).length);
+    expect(nPlugins).toBe(1);
 });
